b8f4b97a083106dd35499f0167b050cf1d0c16e5
Daniel Gamble McAdam <daniel.gamble.mcadam@gmail.com>
2013-09-06 23:08:35 -0500

    Add library class as well as add_book and checkout methods

--- library.rb
+++ library.rb
@@ -1,2 +1,29 @@
+class Library
+
+	def initialize(library)
+		@books = Array.new
+		@library = library
+	end
+
+	def add_book(book)
+		@books.push(book)
+	end
+
+	def checkout_book(book, user)
+		if book.due_date < (Time.now).to_i && user.book_checked_out.length < 2
+			book.status = "Checked Out"
+			book.reader = user.name
+			book due_date = (Time.now + 604800).to_i
+			user.book_checked_out.push(book)
+		elsif book.due_date > (Time.now).to_i
+			puts "The book you selected is already checked out."
+		elsif user.book_checked_out.lenth > 2
+			puts "You cannot check out more than two books."
+		elsif book.status == 'Lost'
+			puts "The book you selected is lost."
+		end
+	end
+
+end
 
 
